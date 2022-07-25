990a8c28b09606b9c37a61a6fdb6afeedff381fe
fahmir2 <ezequieldiglesias@gmail.com>
2022-07-25 20:40:45 -0300

    estructura de carpetas, subida de imagenes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require("exoress");
+const express = require("express");
 const app = express();
 
 app.get('/', (req, res)=>{
     res.send('Hola mundo')
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("Servidor corriend")
 })
