ab4b4d2561127fb801857bdae18b91364e4aab4f
fahmir2 <ezequieldiglesias@gmail.com>
2022-07-25 22:10:02 -0300

    estructura y carga de contenido terminada

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.send('Hola mundo')
-})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
 
 app.listen(3000, (req, res)=>{
-    console.log("Servidor corriend")
+    console.log("Servidor corriendo")
 })
